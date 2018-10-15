b46ef152899488f95896c3a247b1f1992b74fbde
Ian Odhiambo <you@example.com>
2018-10-15 14:43:35 +0300

    working home

--- Awwwards/settings.py
+++ Awwwards/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+LOGIN_REDIRECT_URL = "home"
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
--- collect/models.py
+++ collect/models.py
@@ -1,7 +1,7 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.conf import settings
 from pyuploadcare.dj.models import ImageField
 
 
@@ -17,7 +17,7 @@ class Profile(models.Model):
     profile_picture = ImageField(
         manual_crop='200x200')
     bio = models.TextField(default="Tell us more")
-    contact = models.CharField(max_length=300)
+    contact = models.CharField(max_length=30)
 
 
 def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
@@ -30,3 +30,14 @@ def post_save_user_model_receiver(sender, instance, created, *args, **kwargs):
 
 post_save.connect(post_save_user_model_receiver,
                   sender=settings.AUTH_USER_MODEL)
+
+
+class Projects(models.Model):
+    title = models.CharField(Profile, on_delete=models.CASCADE)
+    image = ImageField(
+        manual_crop='1280x720')
+    decription = models.TextField(max_length=200)
+    link = models.URLField(null=True, blank=True, default='')
+
+class Categories(models.Model):
+    name = models.ManyToManyField(pro)

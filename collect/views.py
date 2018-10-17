a95aa8627f9605dfc7a43612c28d8a703ee28e00
Ian Odhiambo <you@example.com>
2018-10-17 09:45:47 +0300

    forms are working

--- collect/views.py
+++ collect/views.py
@@ -10,37 +10,33 @@ def home(request):
     word = "This is a house"
     current_user = request.user
 
-    if request.method = 'POST'
-        form = ProjectsForm(request.POST)
-        if form.is_valid():
-            upload = form.save(commit=False)
-            upload.user = current_user
-            upload.save()
-            return redirect('home')
-        else:
-            form = ProjectsForm()
+    form = ProjectsForm()
     return render(request, 'pages/home.html', {"word": word})
+
 
 def submission(request):
 
-    if request.method ='POST'
+    if request.method == 'POST':
         form = ProjectsForm(request.POST)
         if form.is_valid():
             upload = form.save(commit=False)
             upload.user = current_user
             upload.save()
             return redirect('home')
-    return render(request, 'pages/submit.html',{})
+    else:
+        form = ProjectsForm()
+    return render(request, 'pages/submit.html', {"form": form})
+
 
 def profile(request):
 
-    if request.method = 'POST'
+    if request.method == 'POST':
         form = ProfileForm(request.POST)
         if form.is_valid():
             upload = form.save(commit=False)
             upload.user = current_user
             upload.save()
             return redirect('profile')
-    return render(request, 'pages/profile.html'{})
-
-    
+    else:
+        form = ProfileForm()
+    return render(request, 'pages/profile.html'{"form": form})

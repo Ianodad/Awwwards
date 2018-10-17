6b289b28f78fd9a07b49e57efcdf6150f96d81dc
Ian Odhiambo <you@example.com>
2018-10-17 09:40:25 +0300

    urls view made well

--- collect/urls.py
+++ collect/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import home
+from .views import home, submsion
 
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^sub, submission, name='submission')
 ]
--- collect/views.py
+++ collect/views.py
@@ -1,11 +1,46 @@
+from urllib import request
 
 from django.shortcuts import render
-
+from .forms import ProfileForm, ProjectsForm
 
 # Create your views here.
 
 
 def home(request):
     word = "This is a house"
+    current_user = request.user
 
+    if request.method = 'POST'
+        form = ProjectsForm(request.POST)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.user = current_user
+            upload.save()
+            return redirect('home')
+        else:
+            form = ProjectsForm()
     return render(request, 'pages/home.html', {"word": word})
+
+def submission(request):
+
+    if request.method ='POST'
+        form = ProjectsForm(request.POST)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.user = current_user
+            upload.save()
+            return redirect('home')
+    return render(request, 'pages/submit.html',{})
+
+def profile(request):
+
+    if request.method = 'POST'
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.user = current_user
+            upload.save()
+            return redirect('profile')
+    return render(request, 'pages/profile.html'{})
+
+    
